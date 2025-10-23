bf32d1e36d8813c8c99ea31a6f65a2c04743591c
TEAtech-TBKs <tolulopeakinwale955@gmail.com>
2025-10-23 22:33:58 +0100

    debug err

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Modern-react-Landing-Page-template-for-Digital-Marketing-Buisness/',
+  // base: '/Modern-react-Landing-Page-template-for-Digital-Marketing-Buisness/',
+  // "homepage": "https://TEAtech-TBKs.github.io/Modern-react-Landing-Page-template-for-Digital-Marketing-Buisness"
+
 })
