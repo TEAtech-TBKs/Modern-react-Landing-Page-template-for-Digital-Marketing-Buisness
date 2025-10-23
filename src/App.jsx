b64175ef8c48927b74e13488311c33ba87d5de0f
TEAtech-TBKs <tolulopeakinwale955@gmail.com>
2025-10-23 18:49:49 +0100

    project testing...

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <div>
      <Routes>
-      <Route path='https://TEAtech-TBKs.github.io/Modern-react-Landing-Page-template-for-Digital-Marketing-Buisness' element={<LandingPage/>}/>
+      <Route path='/' element={<LandingPage/>}/>
       <Route path='/about' element={<About/>}/>
      </Routes>
     </div>
